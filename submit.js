8f2ae5794bf77ea105ecfd7a28fc880271d725ab
sahil-athrij <sahilathrij@gmail.com>
2018-11-22 01:36:11 +0530

    Add files via upload

--- submit.js
+++ submit.js
@@ -8,8 +8,73 @@ var config = {
 };
 firebase.initializeApp(config);
 
+function getevents() {
+    branch = document.getElementById("branch");
+    branch = branch.value;
+    sel = document.getElementById("event");
+    sel.innerHTML = '<option value="">select...</option>';
+    firebase.database().ref('/events/' + branch).once('value').then(function(snapshot) {
+        console.log(snapshot.val());
+        snapshot.forEach(function (child) {
+            console.log(child.val());
+            sel = document.getElementById("event");
+            sel.innerHTML += '<option value='+child.key+">"+child.key+"</option>"
 
 
+
+        })
+        // ...
+    });
+    
+}
+
+function putevent() {
+    branch = document.getElementById("branch");
+    branch = branch.value;
+
+    event = document.getElementById("event");
+    event = event.value;
+
+    if (event == ""){
+        return
+    }
+
+    firebase.database().ref('/events/' + branch+'/'+event).once('value').then(function(snapshot) {
+
+        eve = snapshot.val();
+        nam1 = document.getElementById("name");
+        nam1.value = event;
+
+
+        caption = document.getElementById("caption");
+        caption.value = eve.caption;
+
+        desc = document.getElementById("description");
+        desc.value = eve.description;
+
+        rules = document.getElementById("rules");
+        rules.value = eve.rules;
+
+        loc = document.getElementById("location");
+        loc.value = eve.location;
+
+        c1n = document.getElementById("crd1name");
+        c1n.value = eve.coordinators.crd1.name;
+
+        c1number = document.getElementById("crd1number");
+        c1number.value =  eve.coordinators.crd2.number;
+
+        c2n = document.getElementById("crd2name");
+        c2n.value = eve.coordinators.crd2.name;
+
+        c2number = document.getElementById("crd2number");
+        c2number.value =  eve.coordinators.crd2.number;
+
+
+
+
+    });
+}
 function writedata() {
 
     branch = document.getElementById("branch");
@@ -42,8 +107,6 @@ function writedata() {
     c2number = document.getElementById("crd2number");
     c2number = c2number.value;
 
-    c2number = document.getElementById("crd2number");
-    c2number = c2number.value;
 
     f1 = document.getElementById("image").files[0];
     f2 = document.getElementById("c1image").files[0];
@@ -53,7 +116,7 @@ function writedata() {
     f2name = f2.name.split(".")[1];
     f3name = f3.name.split(".")[1];
 
-    firebase.database().ref('events/'+branch+"/" + nam1).set({
+    firebase.database().ref('events/'+branch+"/" + nam1).update({
 
         caption: caption,
         description:desc,
@@ -66,10 +129,12 @@ function writedata() {
             alert("omg you fucked up one of the most basic tasks in human history")
             // The write failed...
         } else {
-            firebase.database().ref('events/'+branch+"/"  + nam1+"/coordinators/"+c1n).set({
+            firebase.database().ref('events/'+branch+"/"  + nam1+"/coordinators/crd1").update({
+                name:c1n,
                 number:c1number
             });
-            firebase.database().ref('events/'+branch+"/"  + nam1+"/coordinators/"+c2n).set({
+            firebase.database().ref('events/'+branch+"/"  + nam1+"/coordinators/crd2").update({
+                name:c2n,
                 number:c2number
             });
 
@@ -91,3 +156,4 @@ function writedata() {
     });
 
 }
+
